53ad090e874bef6d8b32b2b867624ce58ccf521c
Sean Kirkby <sean.kirkby@niceincontact.com>
2020-05-12 23:02:20 -0600

    implement movies.json

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-export function fetchMovies() {
+let movies = require('./data/movies.json');
+import fetchWithTimeout from './services.js';
 
+export function fetchMovies() {
+    const resolveFunction = () => movies;
+    fetchWithTimeout(1000)
+        .then(resolveFunction);
 }
 
+let moviePromise = fetchMovies();
+
+moviePromise.then(results => { console.log(results); });
