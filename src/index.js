a2edd8b4954eec6bd7d0d4022271247eb7d69424
Sean Kirkby <sean.kirkby@niceincontact.com>
2020-05-12 23:23:10 -0600

    race and all

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import { fetchWithTimeout } from './services';
+import { fetchWithTimeout, fetchMovies, fetchBooks } from './services';
 const movies = require('./data/movies.json');
 
-export function fetchMovies() {
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000)
-        .then(resolveFunction);
+// export function fetchMovies() {
+//     const resolveFunction = () => movies;
+//     return fetchWithTimeout(1000)
+//         .then(resolveFunction);
+// }
+
+// let moviePromise = fetchMovies();
+
+// moviePromise.then(results => { console.log(results); });
+
+function getBooksAndMovies() {
+    return Promise.all([fetchBooks(), fetchMovies()])
+        .then(([books, movies]) => ({
+            books, movies
+        }))
+        .catch(error => console.log('error fetching books and moveis', error));
 }
 
-let moviePromise = fetchMovies();
+const getBooksAndMoviesPromise = getBooksAndMovies();
 
-moviePromise.then(results => { console.log(results); });
+getBooksAndMoviesPromise.then(results => console.log('getBooksAndMoviesPromise', results));
+
+function getBooksOrMovies() {
+    return Promise.race([fetchBooks(), fetchMovies()])
+        .then(results => results)
+        .catch(error => console.log('error waiting for promise race', error));
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+
+getBooksOrMoviesPromise.then(results => console.log(getBooksOrMoviesPromise, results));
